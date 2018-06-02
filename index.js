21282de9589b0c4064137f8807d730acf0cf94dc
Hugh A <harcenea@gmail.com>
2018-06-02 12:48:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const app = "I don't do much." ;
 var kittens = ["Milo", "Otis", "Garfield"];
 var kitten = "Ralph";
 
-function destructivelyAppendKitten(name) { var name = kitten; kittens.push();}
+function destructivelyAppendKitten(name) { var name = kitten; kittens.push(kitten);}
