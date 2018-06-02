7d61f0d6689cf416fdd29afdbf9165f1d18a3835
Hugh A <harcenea@gmail.com>
2018-06-02 12:52:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,6 @@ function destructivelyAppendKitten(kitten) { var name = kitten; kittens.push(kit
 
 function destructivelyPrependKitten(kitten) { var name = kitten;
 var kitten = "Bob"; kittens.unshift(kitten);}
+
+function destructivelyRemoveKitten(kitten)
+{kittens.pop();}
